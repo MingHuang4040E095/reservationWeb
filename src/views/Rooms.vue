0bdd6762f2a63eada41f48698d0050a9cf542534
MingHuang1220 <syuminghuang@gmail.com>
2021-10-02 00:01:57 +0800

    房間介紹頁加入萬年曆組件

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -2,8 +2,40 @@
     <div>
         <v-row style="background:#D1C8C3;">
             <v-container>
-                <v-row class="px-10 py-3">
-                    datePicker
+                <v-row class="px-10 py-3" align="center">
+                    <v-col cols="6" md="2">
+                        <button class="date-block__btn" @click="selectDateRange">
+                            <span style="letter-spacing: 2px;">{{ startDate }}</span>
+                            <v-icon right color="#000000">
+                                mdi-calendar-range
+                            </v-icon>
+                        </button>
+                    </v-col>
+                    <v-col cols="6" md="2">
+                        <button class="date-block__btn" @click="selectDateRange">
+                            <span style="letter-spacing: 2px;">{{ endDate }}</span>
+                            <v-icon right color="#000000">
+                                mdi-calendar-range
+                            </v-icon>
+                        </button>
+                    </v-col>
+                    <v-col cols="6" md="2">
+                        <div class="date-block__count d-flex">
+                            <button @click="adultsNumCount('minus')">-</button>
+                            <div>{{ adultsNum }}</div>
+                            <button @click="adultsNumCount('plus')">+</button>
+                        </div>
+                    </v-col>
+                    <v-col cols="6" md="2">
+                        <div class="date-block__count d-flex">
+                            <button @click="kidsNumCount('minus')">-</button>
+                            <div>{{ kidsNum }}</div>
+                            <button @click="kidsNumCount('plus')">+</button>
+                        </div>
+                    </v-col>
+                    <v-col cols="6" md="2">
+                        <button class="date-block__btn-search" style="background:#000000;color:#ffffff;">Search</button>
+                    </v-col>
                 </v-row>
             </v-container>
         </v-row>
@@ -56,16 +88,19 @@
             </v-container>
         </v-row>
         <Detail :dialogVisible="dialogVisible" @setDialogVisible="setDialogVisible" />
+        <Calendar v-show="visibleCalendar" @close="closeCalendar" @setDate="getDate" />
     </div>
 </template>
 <script>
 import { mapState } from 'vuex'
 import ImageCard from '@/components/ImageCard.vue'
 import Detail from '@/components/Detail.vue'
+import Calendar from '@/components/Calendar.vue'
 export default {
     components: {
         ImageCard,
         Detail,
+        Calendar,
     },
     computed: {
         ...mapState(['roomTypeList']),
@@ -82,12 +117,40 @@ export default {
                 require('@/assets/image/rooms/wifi.svg'),
             ],
             dialogVisible: true,
+            adultsNum: 0,
+            kidsNum: 0,
+            visibleCalendar: false, //是否可見萬年曆
+            startDate: '開始日期', //開始日期
+            endDate: '結束日期', //結束日期
         }
     },
     methods: {
         //設定詳細資料 dialog 的顯示狀態
         setDialogVisible(newStatus) {
             this.dialogVisible = newStatus
+        },
+        adultsNumCount(type) {
+            if (type === 'plus') this.adultsNum++
+            else this.adultsNum--
+            if (this.adultsNum < 0) this.adultsNum = 0
+        },
+        kidsNumCount(type) {
+            if (type === 'plus') this.kidsNum++
+            else this.kidsNum--
+            if (this.kidsNum < 0) this.kidsNum = 0
+        },
+        //選擇日期範圍
+        selectDateRange() {
+            this.visibleCalendar = true
+        },
+        //關閉萬年曆
+        closeCalendar(status) {
+            this.visibleCalendar = status
+        },
+        //取得子組件傳過來的日期
+        getDate(dataRange) {
+            this.startDate = dataRange.startDate
+            this.endDate = dataRange.endDate
         },
     },
 }
@@ -106,6 +169,55 @@ export default {
         // >.v-image__image {
         //     background-size: contain;
         // }
+    }
+}
+.date-block__btn {
+    display: flex;
+    background: #ffffff;
+    width: 100%;
+    // max-width: 150px;
+    padding: 5px 15px;
+    justify-content: space-between;
+    align-items: center;
+    &:focus {
+        outline: none;
+    }
+}
+.date-block__btn-search {
+    display: inline-block;
+    outline: none;
+    font-weight: bold;
+    letter-spacing: 1px;
+    border: 2px solid #000000;
+    color: #000000;
+    background: transparent;
+    padding: 0.5rem 1.5rem;
+}
+.date-block__count {
+    height: 30px;
+    background: #ffffff;
+    display: flex;
+    justify-content: space-between;
+    > div,
+    > button {
+        width: 30px;
+        height: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    & > button:first-child,
+    & > button:last-child {
+        color: #ffffff;
+        background: #000;
+
+        &:focus {
+            border: none;
+            outline: none;
+        }
+    }
+    & + .date-block__count {
+        margin-left: 1rem;
     }
 }
 </style>
